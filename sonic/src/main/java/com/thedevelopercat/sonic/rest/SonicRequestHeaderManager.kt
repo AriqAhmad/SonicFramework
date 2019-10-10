7ff2d7f2453d90bd1354c7c7c7bd1dee25d7b26d
Ariq Ahmad <ariqahmad@gmail.com>
2019-10-10 19:43:01 +0530

    Added request header manager

---
+++ sonic/src/main/java/com/thedevelopercat/sonic/rest/SonicRequestHeaderManager.kt
@@ -0,0 +1,46 @@
+package com.thedevelopercat.sonic.rest
+
+class SonicRequestHeaderManager {
+
+    protected val headers = HashMap<String, String>()
+
+    fun getHeaderMap(): HashMap<String, String> {
+        return headers
+    }
+
+    fun addOrUpdateHeader(key: String, value: String){
+        headers[key] = value
+    }
+
+    fun removeHeaderForKey(key: String){
+        headers.remove(key)
+    }
+
+    fun containsValue(value: String): Boolean {
+        return getAllValues().contains(value)
+    }
+
+    fun containsKey(key: String): Boolean {
+        return getAllKeys().contains(key)
+    }
+
+    fun clearHeader(){
+        headers.clear()
+    }
+
+    fun getAllKeys(): MutableSet<String> {
+        return headers.keys
+    }
+
+    fun getAllValues(): MutableCollection<String> {
+        return headers.values
+    }
+
+    fun isEmpty(): Boolean {
+        return headers.keys.isEmpty()
+    }
+
+    fun isNotEmpty(): Boolean {
+        return !isEmpty()
+    }
+}
